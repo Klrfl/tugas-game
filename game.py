73647da4c5b9024606b1ee7df5348eb79f7a7051
Efraim Munthe <efraimmunthe101@gmail.com>
2022-11-29 11:47:06 +0700

    update

--- game.py
+++ game.py
@@ -8,12 +8,16 @@ screen = pygame.display.set_mode((1280, 700))
 
 # set title
 pygame.display.set_caption('Mario clone')
+
+# set surfaces
 test_surface = pygame.Surface((100, 200))
 test_surface.fill("Red")
 
+test_font = pygame.font.Font(None, 50)
+text_surface = test_font.render("tes", False, 'White')
+
 # clock obj for framerate
 clock = pygame.time.Clock()
-
 
 running = True
 while running:
@@ -22,6 +26,6 @@ while running:
       running = False
   
   screen.blit(test_surface, (200, 100))
-
+  screen.blit(text_surface, (300, 500))
   pygame.display.update()
   clock.tick(40)
