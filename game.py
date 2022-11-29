58041db5da12779528d4a4cea7678c0527eb5a9a
Efraim Munthe <efraimmunthe101@gmail.com>
2022-11-29 11:10:03 +0700

    update

--- game.py
+++ game.py
@@ -3,10 +3,25 @@ from library.Setting import *
 
 pygame.init()
 
-screen = pygame.display.set((800, 600))
+# set screen
+screen = pygame.display.set_mode((1280, 700))
+
+# set title
+pygame.display.set_caption('Mario clone')
+test_surface = pygame.Surface((100, 200))
+test_surface.fill("Red")
+
+# clock obj for framerate
+clock = pygame.time.Clock()
+
 
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+  
+  screen.blit(test_surface, (200, 100))
+
+  pygame.display.update()
+  clock.tick(40)
