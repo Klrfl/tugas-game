d9ee67d0cd04ec9fb765fbc12a8eb8a8cbf94737
Efraim Munthe <efraimmunthe101@gmail.com>
2022-11-29 19:30:14 +0700

    unfinished debugging in tile.py and game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 from library.Setting import *
 from library.tile import Tile
-from library.map import Map
+# from library.map import Map
 
 pygame.init()
 
@@ -13,17 +13,17 @@ pygame.display.set_caption('Mario clone')
 
 # set surfaces
 sky_surface = pygame.image.load("asset/Sky.png")
-
 ground_surface = pygame.image.load("asset/Ground.png")
 
+# text
 test_font = pygame.font.Font(None, 50)
 text_surface = test_font.render("tes", False, 'White')
 
+test_tile = pygame.sprite.Group(Tile((200,100),200))
+# map = Map(map,screen)
+
 # clock obj for framerate
 clock = pygame.time.Clock()
-
-t = pygame.sprite.Group(Tile(200,100),200)
-map = Map(map,screen)
 
 
 running = True
@@ -31,12 +31,12 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-  """
+  
   screen.blit(sky_surface, (0, 0))
   screen.blit(ground_surface, (0, 450))
-  """
   
-  map.exec()
+  test_tile.draw(screen)
+  
+  # map.exec()
   pygame.display.update()
   clock.tick(40)
-  t.draw(screen)
--- library/tile.py
+++ library/tile.py
@@ -1,8 +1,8 @@
 import pygame
 
-class Tile(pygame.sprite.Sprite()):
-  def __init__(self,pos,ukuran):
+class Tile(pygame.sprite.Sprite):
+  def __init__(self, pos, ukuran):
     super().__init__()
-    self.Surface = pygame.Surface((size,size))
-    self.Surface.fill("Brown")
-    self.rect = self.Surface.get_rect(topl = pos)
+    self.image = pygame.Surface((ukuran, ukuran))
+    self.image.fill("Brown")
+    self.rect = self.image.get_rect(topleft = pos)
